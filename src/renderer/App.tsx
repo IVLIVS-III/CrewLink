06b81e5f77bc2cfdbb12a73eb9d6446588daa520
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2020-12-27 16:20:21 +0100

    Update modified version name.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -93,7 +93,7 @@ export default function App() {
 		<GameStateContext.Provider value={gameState}>
 			<SettingsContext.Provider value={settings}>
 				<div className="titlebar">
-					<span className="title">CrewLink{appVersion}-m.3</span>
+					<span className="title">CrewLink{appVersion}-m.4</span>
 					<svg className="titlebar-button settings" onClick={() => setSettingsOpen(!settingsOpen)} enableBackground="new 0 0 24 24" viewBox="0 0 24 24" fill="#868686" width="20px" height="20px">
 						<g>
 							<path d="M0,0h24v24H0V0z" fill="none" />
