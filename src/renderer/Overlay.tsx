4f8082d602d9b6e70ec0cfa27b254e257697221d
IVLIVS <ivlivs.coding@gmail.com>
2020-12-30 19:07:29 +0100

    Fix linter issues.

--- src/renderer/Overlay.tsx
+++ src/renderer/Overlay.tsx
@@ -17,7 +17,7 @@ interface SocketIdMap {
 }
 
 export default function Overlay() {
-	const [status, setStatus] = useState("WAITING");
+	const [status, setStatus] = useState('WAITING');
 	const [gameState, setGameState] = useState<AmongUsState>({} as AmongUsState);
 	const [settings, setSettings] = useState<ISettings>({} as ISettings);
 	const [socketPlayerIds, setSocketPlayerIds] = useState<SocketIdMap>({});
@@ -26,30 +26,47 @@ export default function Overlay() {
 	const [otherDead, setOtherDead] = useState<OtherDead>({});
 	const myPlayer = useMemo(() => {
 		if (!gameState || !gameState.players) return undefined;
-		else return gameState.players.find(p => p.isLocal);
+		else return gameState.players.find((p) => p.isLocal);
 	}, [gameState]);
 
 	const relevantPlayers = useMemo(() => {
 		let relevantPlayers: Player[];
-		if (!gameState || !gameState.players || gameState.lobbyCode === 'MENU' || !myPlayer) relevantPlayers = [];
-		else relevantPlayers = gameState.players.filter(p => (
-			(Object.values(socketPlayerIds).includes(p.id) || p.isLocal) &&
-			((!myPlayer.isDead && !otherDead[p.id]) || myPlayer.isDead)
-		));
+		if (
+			!gameState ||
+			!gameState.players ||
+			gameState.lobbyCode === 'MENU' ||
+			!myPlayer
+		)
+			relevantPlayers = [];
+		else
+			relevantPlayers = gameState.players.filter(
+				(p) =>
+					(Object.values(socketPlayerIds).includes(p.id) || p.isLocal) &&
+					((!myPlayer.isDead && !otherDead[p.id]) || myPlayer.isDead)
+			);
 		return relevantPlayers;
 	}, [gameState]);
 
 	let talkingPlayers: Player[];
-	if (!gameState || !gameState.players || gameState.lobbyCode === 'MENU' || !myPlayer) talkingPlayers = [];
-	else talkingPlayers = gameState.players.filter(p => (otherTalking[p.id] || (p.isLocal && talking)));
+	if (
+		!gameState ||
+		!gameState.players ||
+		gameState.lobbyCode === 'MENU' ||
+		!myPlayer
+	)
+		talkingPlayers = [];
+	else
+		talkingPlayers = gameState.players.filter(
+			(p) => otherTalking[p.id] || (p.isLocal && talking)
+		);
 
 	useEffect(() => {
 		if (gameState.gameState === GameState.LOBBY) {
 			setOtherDead({});
 		} else if (gameState.gameState !== GameState.TASKS) {
 			if (!gameState.players) return;
-			setOtherDead(old => {
-				for (let player of gameState.players) {
+			setOtherDead((old) => {
+				for (const player of gameState.players) {
 					old[player.id] = player.isDead || player.disconnected;
 				}
 				return { ...old };
@@ -58,7 +75,10 @@ export default function Overlay() {
 	}, [gameState.gameState]);
 
 	useEffect(() => {
-		const onOverlaySettings = (_: Electron.IpcRendererEvent, newSettings: any) => {
+		const onOverlaySettings = (
+			_: Electron.IpcRendererEvent,
+			newSettings: any
+		) => {
 			setSettings(newSettings);
 		};
 
@@ -66,32 +86,39 @@ export default function Overlay() {
 			setStatus(state);
 		};
 
-		const onOverlayGameState = (_: Electron.IpcRendererEvent, newState: AmongUsState) => {
+		const onOverlayGameState = (
+			_: Electron.IpcRendererEvent,
+			newState: AmongUsState
+		) => {
 			setGameState(newState);
 		};
 
-		const onOverlaySocketIds = (_: Electron.IpcRendererEvent, ids: SocketIdMap) => {
+		const onOverlaySocketIds = (
+			_: Electron.IpcRendererEvent,
+			ids: SocketIdMap
+		) => {
 			setSocketPlayerIds(ids);
 		};
 
-
-		const onOverlayTalkingSelf = (_: Electron.IpcRendererEvent, talking: boolean) => {
+		const onOverlayTalkingSelf = (
+			_: Electron.IpcRendererEvent,
+			talking: boolean
+		) => {
 			setTalking(talking);
 		};
 
 		const onOverlayTalking = (_: Electron.IpcRendererEvent, id: number) => {
-			setOtherTalking(old => ({
+			setOtherTalking((old) => ({
 				...old,
-				[id]: true
+				[id]: true,
 			}));
 		};
 
 		const onOverlayNotTalking = (_: Electron.IpcRendererEvent, id: number) => {
-			setOtherTalking(old => ({
+			setOtherTalking((old) => ({
 				...old,
-				[id]: false
+				[id]: false,
 			}));
-
 		};
 
 		ipcRenderer.on('overlaySettings', onOverlaySettings);
@@ -109,37 +136,45 @@ export default function Overlay() {
 			ipcRenderer.off('overlayTalkingSelf', onOverlayTalkingSelf);
 			ipcRenderer.off('overlayTalking', onOverlayTalking);
 			ipcRenderer.off('overlayNotTalking', onOverlayNotTalking);
-		}
+		};
 	}, []);
 
-	document.body.style.backgroundColor = "rgba(255, 255, 255, 0)";
-	document.body.style.paddingTop = "0";
+	document.body.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+	document.body.style.paddingTop = '0';
 
-	var baseCSS: any = {
-		backgroundColor: "rgba(0, 0, 0, 0.85)",
-		width: "100px",
-		borderRadius: "8px",
-		position: "relative",
-		marginTop: "-16px",
-		paddingLeft: "8px",
+	const baseCSS: any = {
+		backgroundColor: 'rgba(0, 0, 0, 0.85)',
+		width: '100px',
+		borderRadius: '8px',
+		position: 'relative',
+		marginTop: '-16px',
+		paddingLeft: '8px',
 	};
-	var topArea = <p><b style={{ color: "#9b59b6" }}>CrewLink</b> ({status})</p>
-	var playersCSS: any = {}
-	var playerList: Player[] = [];
-	if (gameState.players && gameState.gameState != GameState.MENU) playerList = relevantPlayers;
+	let topArea = (
+		<p>
+			<b style={{ color: '#9b59b6' }}>CrewLink</b> ({status})
+		</p>
+	);
+	const playersCSS: any = {};
+	let playerList: Player[] = [];
+	if (gameState.players && gameState.gameState != GameState.MENU)
+		playerList = relevantPlayers;
 
-	if (gameState.gameState == GameState.UNKNOWN || gameState.gameState == GameState.MENU) {
-		baseCSS["left"] = "8px";
-		baseCSS["top"] = "60px";
+	if (
+		gameState.gameState == GameState.UNKNOWN ||
+		gameState.gameState == GameState.MENU
+	) {
+		baseCSS['left'] = '8px';
+		baseCSS['top'] = '60px';
 	} else {
-		baseCSS["paddingTop"] = "8px";
-		baseCSS["paddingLeft"] = "0px";
-		baseCSS["width"] = "800px";
-		baseCSS["backgroundColor"] = "rgba(0, 0, 0, 0.5)";
+		baseCSS['paddingTop'] = '8px';
+		baseCSS['paddingLeft'] = '0px';
+		baseCSS['width'] = '800px';
+		baseCSS['backgroundColor'] = 'rgba(0, 0, 0, 0.5)';
 		if (settings.overlayPosition == 'top') {
-			baseCSS["marginLeft"] = "auto";
-			baseCSS["marginRight"] = "auto";
-			baseCSS["marginTop"] = "0px";
+			baseCSS['marginLeft'] = 'auto';
+			baseCSS['marginRight'] = 'auto';
+			baseCSS['marginTop'] = '0px';
 
 			playersCSS['display'] = 'flex';
 			playersCSS['flex-direction'] = 'row';
@@ -147,51 +182,70 @@ export default function Overlay() {
 			playersCSS['justifyContent'] = 'center';
 			playersCSS['alignItems'] = 'center';
 		} else if (settings.overlayPosition == 'bottom_left') {
-			baseCSS["position"] = "absolute";
-			baseCSS["bottom"] = "0px";
-			baseCSS["backgroundColor"] = "rgba(0, 0, 0, 0.35)";
-			baseCSS["width"] = null;
+			baseCSS['position'] = 'absolute';
+			baseCSS['bottom'] = '0px';
+			baseCSS['backgroundColor'] = 'rgba(0, 0, 0, 0.35)';
+			baseCSS['width'] = null;
 
-			playersCSS["justifyContent"] = "left"
-			playersCSS["alignItems"] = "left"
+			playersCSS['justifyContent'] = 'left';
+			playersCSS['alignItems'] = 'left';
 		}
 		topArea = <></>;
-		if ((settings.compactOverlay || gameState.gameState == GameState.TASKS) && playerList) {
+		if (
+			(settings.compactOverlay || gameState.gameState == GameState.TASKS) &&
+			playerList
+		) {
 			playerList = talkingPlayers;
-			baseCSS["backgroundColor"] = "rgba(0, 0, 0, 0)";
+			baseCSS['backgroundColor'] = 'rgba(0, 0, 0, 0)';
 		}
 	}
 
-	var playerArea: JSX.Element = <></>;
+	let playerArea: JSX.Element = <></>;
 	if (playerList) {
-		playerArea = <div className="otherplayers" style={playersCSS}>
-			{
-				playerList.map(player => {
-					const connected = Object.values(socketPlayerIds).includes(player.id) || player.isLocal;
-					let name = settings.compactOverlay ? "" : <span><small>{player.name}</small></span>
+		playerArea = (
+			<div className="otherplayers" style={playersCSS}>
+				{playerList.map((player) => {
+					const connected =
+						Object.values(socketPlayerIds).includes(player.id) ||
+						player.isLocal;
+					const name = settings.compactOverlay ? (
+						''
+					) : (
+							<span>
+								<small>{player.name}</small>
+							</span>
+						);
 					return (
-						<div key={player.id} style={{ width: "60px", textAlign: "center" }}>
-							<div style={{ paddingLeft: "5px" }}>
-								<Avatar key={player.id} player={player}
-									talking={!connected || otherTalking[player.id] || (player.isLocal && talking)}
+						<div key={player.id} style={{ width: '60px', textAlign: 'center' }}>
+							<div style={{ paddingLeft: '5px' }}>
+								<Avatar
+									key={player.id}
+									player={player}
+									talking={
+										!connected ||
+										otherTalking[player.id] ||
+										(player.isLocal && talking)
+									}
 									borderColor={connected ? '#2ecc71' : '#c0392b'}
-									isAlive={(!player.isLocal && !otherDead[player.id]) || (player.isLocal && !player.isDead)}
-									size={50} />
+									isAlive={
+										(!player.isLocal && !otherDead[player.id]) ||
+										(player.isLocal && !player.isDead)
+									}
+									size={50}
+								/>
 							</div>
 							{name}
 						</div>
 					);
-				})
-			}
-		</div>
+				})}
+			</div>
+		);
 	}
-
-
 
 	return (
 		<div style={baseCSS}>
 			{topArea}
 			{playerArea}
 		</div>
-	)
-} 
+	);
+}
