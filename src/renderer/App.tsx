4c48aeb49f32b2d2ff954d253b4fa433acef3fe0
IVLIVS <ivlivs.coding@gmail.com>
2020-12-31 16:46:00 +0100

    Update version indicator.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -84,7 +84,7 @@ const TitleBar: React.FC<TitleBarProps> = function ({
 	const classes = useStyles();
 	return (
 		<div className={classes.root}>
-			<span className={classes.title}>CrewLink{appVersion}-m.1</span>
+			<span className={classes.title}>CrewLink{appVersion}-m.2</span>
 			<IconButton
 				className={classes.button}
 				style={{ left: 0 }}
